6ee9f6a721f2c24dfb6a66ab3bce42cf6f839a3a
Alex <1638309+AlexScotland@users.noreply.github.com>
2022-08-12 12:31:32 -0400

    adds missed files from changes

--- src/theory/factories/scale_factory.py
+++ src/theory/factories/scale_factory.py
@@ -8,5 +8,4 @@ class ScaleFactory():
         return Scale(root_note, [2, 2, 1, 2, 2 ,2, 1], NoteFactory.create_piano_roll()) 
     
     def create_scale_from_key(root_note: Note, key: IKey) -> Scale:
-        
-        return Scale(root_note, [2, 2, 1, 2, 2 ,2, 1], NoteFactory.create_piano_roll()) 
+        return Scale(root_note, key.octave_steps, NoteFactory.create_piano_roll()) 
--- src/theory/models/__init__.py
+++ src/theory/models/__init__.py
@@ -1,2 +1,4 @@
 from .note import Note
 from .scale import Scale
+from .chord import Chord
+from .key import Key
