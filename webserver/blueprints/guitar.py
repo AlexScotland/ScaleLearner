54e2d0817922ffa679c975038162e8834d794a49
Alex Scotland <alexander.scotland@bell.ca>
2022-12-05 13:04:44 -0500

    adds note to api

--- webserver/blueprints/guitar.py
+++ webserver/blueprints/guitar.py
@@ -6,6 +6,9 @@ guitar_blueprint = Blueprint('api', __name__, url_prefix='/api')
 
 @guitar_blueprint.route('/guitar/', methods=['GET'])
 def guitar():
+    note = request.args.get('note')
+    if not note:
+        return "Invalid Note Given"
     guitar = APIGuitar()
-    e = Note("E", 2)
+    e = Note(note.upper(), 2)
     return guitar.get_all_note_positions_for_api(e)
